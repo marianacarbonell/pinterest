4315f56b2ea5a35d0ec1a9eaf9ebda13f6a3825b
marianacarbonell <marianacarbonellm@gmail.com>
2019-04-08 14:08:37 -0400

    primer commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+
+firebase.initializeApp({
+        apiKey: "AIzaSyDCzEyMWwfDzAAivUESp8vE2Ou61oCzlN8",
+        authDomain: "comentarios-cd6c9.firebaseapp.com",
+        databaseURL: "https://comentarios-cd6c9.firebaseio.com",
+        projectId: "comentarios-cd6c9",
+        storageBucket: "comentarios-cd6c9.appspot.com",
+        messagingSenderId: "815230709622"
+    
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
