bf8049230ae077b4bc71458173c18460c9a587c5
marianacarbonell <marianacarbonellm@gmail.com>
2019-04-10 11:39:08 -0400

    colocando los elementos del navbar en el mismo componente

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,22 +3,28 @@ import './Navbar.css';
 import './Img.css';
 import RigthButtons from './RigthButtons';
 
-                                                                                                                                                  
+
 class Navbar extends Component {
     render() {
         return (
             <div className="navbar">
-                <div className="Logo">
-                <img src="https://cdn.freebiesupply.com/logos/large/2x/pinterest-1-logo-png-transparent.png" className="image" alt=""/>
-                </div>
-                <div className="Search">
-                    <input type="text" placeholder="Buscar" />
-                </div>
-                <RigthButtons/>
-            </div> 
-                )
-        
-            }
-        }
-        
+
+                <navbar>
+                    <url>
+                        <img src="https://cdn.freebiesupply.com/logos/large/2x/pinterest-1-logo-png-transparent.png" className="image" alt="" />
+                        <input className="Search" placeholder="Buscar"></input>
+                        <a className="Home">Inicio</a>
+                        <a className="Follow">Siguiendo</a>
+                        <a className="Name">Mariana</a>
+                        <i className="fas fa-comment-dots"></i>
+                        <i className="fas fa-bell"></i>
+                        <i className="fas fa-ellipsis-h"></i>
+                    </url>
+                </navbar>
+            </div>
+        )
+
+    }
+}
+
 export default Navbar;
