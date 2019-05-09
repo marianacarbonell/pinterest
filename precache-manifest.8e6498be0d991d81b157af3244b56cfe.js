e0ae6f64618c8994e6a75d66edbf439e3ed47551
marianacarbonell <marianacarbonellm@gmail.com>
2019-05-09 17:27:06 -0400

    Updates

---
+++ precache-manifest.8e6498be0d991d81b157af3244b56cfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e25fe8f2b8775529351e",
+    "url": "/marianacarbonell/pinterest/static/js/runtime~main.ca945069.js"
+  },
+  {
+    "revision": "32064d8637f35b50690d",
+    "url": "/marianacarbonell/pinterest/static/js/main.464fdfca.chunk.js"
+  },
+  {
+    "revision": "bc28ddc73b8fe8657331",
+    "url": "/marianacarbonell/pinterest/static/js/2.68b97e70.chunk.js"
+  },
+  {
+    "revision": "32064d8637f35b50690d",
+    "url": "/marianacarbonell/pinterest/static/css/main.ea41d531.chunk.css"
+  },
+  {
+    "revision": "a285408279f82a0d4f39c6dfb716e1c7",
+    "url": "/marianacarbonell/pinterest/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/marianacarbonell/pinterest/precache-manifest.8e6498be0d991d81b157af3244b56cfe.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/marianacarbonell/pinterest/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
